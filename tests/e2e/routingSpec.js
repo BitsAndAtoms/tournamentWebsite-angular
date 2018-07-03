cf6782c483ce04e9e376e042cfdba99140b7dd62
Siddhartha Gupta <sgupta2@my.westga.edu>
2018-07-03 03:00:22 +0000

    wrote tests to check navigation to registration page and tournament page

---
+++ tests/e2e/routingSpec.js
@@ -0,0 +1,32 @@
+function dumpHtml() {
+  browser.getPageSource().then(function(source) {
+    console.log(source);
+  });
+}
+
+
+describe('Routing Test to navigate to welcome.html', function() {
+  it('should take user to welcome page after loading main.html', function() { // Open the list of teams page browser . get ( '/' );
+    browser.get('/main.html');
+    expect((element(by.tagName('h2'))).getText()).toEqual('Welcome');
+  });
+});
+
+describe('Routing Test for registration link', function() {
+  it('should show teams on the first page', function() { // Open the list of teams page browser . get ( '/' );
+    browser.get('/main.html');
+    element(by.id('registrationLink')).click();
+    expect((element(by.tagName('h2'))).getText()).toEqual('Register Players');
+  });
+});
+
+describe('Routing Test for tournament link', function() {
+  it('should show teams on the first page', function() { // Open the list of teams page browser . get ( '/' );
+    browser.get('/main.html');
+    element(by.id('tournamentLink')).click();
+    element.all(by.tagName('h2')).get(0).getText().then(function(text) {
+      console.log(text);
+    });
+    expect((element.all(by.tagName('h2'))).get(0).getText()).toEqual('Tournament');
+  });
+});
