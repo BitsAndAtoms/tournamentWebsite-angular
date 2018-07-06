4264018edb812e63166c21ffe60432832b5a6ce9
Siddhartha Gupta <sgupta2@my.westga.edu>
2018-07-06 02:20:16 +0000

    trigger error on registration with 7 players

---
+++ tests/e2e/registrationErrorWhenSubmit7PlayersSpec0.js
@@ -0,0 +1,26 @@
+function dumpHtml() {
+  browser.getPageSource().then(function(source) {
+    console.log(source);
+  });
+}
+
+
+
+describe('Succeess scenario from loading to tournament complete', function() {
+  it('should complete a tournament successfully', function() { // Open the list of teams page browser . get ( '/' );
+    browser.get('/main.html');
+    expect((element(by.tagName('h2'))).getText()).toEqual('Welcome');
+    element(by.id('registrationLink')).click();
+    expect((element(by.tagName('h2'))).getText()).toEqual('Register Players');
+    element.all(by.id('Contestant_1')).clear().sendKeys('Beethoven');
+    element.all(by.id('Contestant_2')).clear().sendKeys('Mozart');
+    element.all(by.id('Contestant_3')).clear().sendKeys('Tchaikovsky');
+    element.all(by.id('Contestant_4')).clear().sendKeys('Kalidas');
+    element.all(by.id('Contestant_5')).clear().sendKeys('Shelly');
+    element.all(by.id('Contestant_6')).clear().sendKeys('Yeats');
+    element.all(by.id('Contestant_7')).clear().sendKeys('Rachmanioff');
+    element(by.id('submitRosterButton')).click();
+    expect(element(by.id('error')).getText()).toEqual('Must seed bracket with either 2, 4, or 8 players');
+
+  });
+});
